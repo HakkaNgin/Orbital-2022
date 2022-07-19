65d27d60557c0d11360974c4fd4bdf52c4014584
Daniel Luo <hurinturin123@gmail.com>
2022-07-19 14:54:52 +0800

    No changes to Models

---
+++ lib_milestone_three/models/profile_constants.dart
@@ -0,0 +1,9 @@
+// import 'package:log_in/screens/home/home.dart';
+//
+// class ProfileConstants {
+//
+//   const ProfileConstants({required this.userId});
+//
+//   final String userId = homeCurrentUserID;
+//
+// }
